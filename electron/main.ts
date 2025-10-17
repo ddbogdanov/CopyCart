4d8a027838af55615f035217e95e7a24eb733447
Dennis Bogdanov <dennis.d.bogdanov@gmail.com>
2025-10-17 02:32:16 -0500

    update squirrel events

--- electron/main.ts
+++ electron/main.ts
@@ -102,32 +102,26 @@ function handleSquirrelEvent() {
 	var squirrelCommand = process.argv[1];
 	switch (squirrelCommand) {
 		case '--squirrel-install':
+		console.log('Squirrel Event: Install');
+		app.quit();
+		return true;
+
 		case '--squirrel-updated':
-
-		// Optionally do things such as:
-		//
-		// - Install desktop and start menu shortcuts
-		// - Add your .exe to the PATH
-		// - Write to the registry for things like file associations and
-		//   explorer context menus
-
-		// Always quit when done
+		console.log('Squirrel Event: Updated');
 		app.quit();
-
 		return true;
+
 		case '--squirrel-uninstall':
-		// Undo anything you did in the --squirrel-install and
-		// --squirrel-updated handlers
-
-		// Always quit when done
+		console.log('Squirrel Event: Uninstall');
 		app.quit();
-
 		return true;
+
 		case '--squirrel-obsolete':
-		// This is called on the outgoing version of your app before
-		// we update to the new version - it's the opposite of
-		// --squirrel-updated
+		console.log('Squirrel Event: Obsolete');
 		app.quit();
 		return true;
-	}
+
+		default:
+		return false; // No Squirrel event detected
+  	}
 }
